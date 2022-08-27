112029ca0d65cac833d0999295c21297441191d9
Zelimkhan <digiev2002@mail.ru>
2022-08-27 18:29:04 +0300

    Wrote the general structure

---
+++ soldier.js
@@ -0,0 +1,13 @@
+const soldier = {
+    name: 'Халк',
+    health: 10,
+    weapon: {
+        name: 'ПП',
+        bullet: 30,
+    },
+    supplies: 3,
+
+    fire: function () { },
+    recharge: function () { },
+    hurt: function () { },
+};
