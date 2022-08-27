d62bf6143a597cc8129f33e507e77cdea88eff84
Zelimkhan <digiev2002@mail.ru>
2022-08-27 19:07:36 +0300

    All subtasks completed! Added methods for obj (fire, recharge, hurt)

--- soldier.js
+++ soldier.js
@@ -7,7 +7,33 @@ const soldier = {
     },
     supplies: 3,
 
-    fire: function () { },
-    recharge: function () { },
-    hurt: function () { },
+    fire: function () { 
+      if(this.weapon.bullet > 0) {
+         this.weapon.bullet--;
+          console.log('Fire-fire! Die creatures') 
+    }
+      else {
+         console.log('Magazine empty, reload your weapon')
+      }
+    },
+
+    recharge: function () { 
+        
+        if(this.supplies > 0) {
+        this.weapon.bullet = 30;
+        this.supplies -= 1;
+            console.log('Reloading....') ;
+        } else {
+            console.log("Supplies run out!! I don't have supplies :(")
+        }
+    },
+    hurt: function () {
+        if (this.health > 0) {
+            this.health -= 1
+            console.log("I got shot, but i haven't give up!")
+        } else {
+            console.log('You dead...')
+            return false
+        }
+     },
 };
